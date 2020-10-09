9e7e5817862474242ac3d239dc8672f1949418bc
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-10-08 19:00:17 -0700

    finished wave 2 (with tests)

--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -1,15 +1,57 @@
-require 'minitest/autorun'
+require_relative 'test_helper'
 
-class WORKSPACETest < MiniTest::Unit::TestCase
-  def setup
-    # Do nothing
+
+describe Workspace do
+
+
+  describe "initializer" do
+    it 'It can create a Workspace instance' do
+       VCR.use_cassette(" users and channel") do
+         workspace = Workspace.new
+      expect(workspace).must_be_instance_of Workspace
+      end
+    end
   end
 
-  def teardown
-    # Do nothing
+  describe "select_user" do
+
+    it "It can select a user instance" do
+      VCR.use_cassette(" workspacce users") do
+        workplace = Workspace.new
+        selected = workplace.select_user("slackbot")
+
+      expect(selected).must_be_instance_of User
+      expect(selected.name).must_equal "slackbot"
+      expect(selected.real_name).must_equal "Slackbot"
+      expect(selected.slack_id).must_equal "USLACKBOT"
+      end
+    end
+
   end
 
-  def test
-    skip 'Not implemented'
+  describe "select_channel" do
+    it "selects the correct channel" do
+      VCR.use_cassette(" workspacce channels") do
+        workplace = Workspace.new
+        selected = workplace.select_channel("general")
+
+        expect(selected.name).must_equal "general"
+        expect(selected.slack_id).must_equal "C0165N9BX3M"
+      end
+    end
   end
+
+  describe "show details" do
+    it "shows details selected user/channel" do
+      VCR.use_cassette(" workspacce channels") do
+        workplace = Workspace.new
+
+        expect(workplace.selected).must_be_nil
+
+      end
+    end
+  end
+
 end
+
+
